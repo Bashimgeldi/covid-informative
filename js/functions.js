a4e334543642da234c2dcf2ee2a58af81b3870f9
Shohin Aslamshoev <54887613+shohinav@users.noreply.github.com>
2021-06-18 12:42:40 +0800

    initial push

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,32 @@
-const openMenu = document.getElementById('open-menu');
-const navLinks = document.getElementById('nav-links');
+var scrollPosition = window.scrollY;
+var logoContainer = document.querySelector(".header");
 
-openMenu.addEventListener('click', () => {
-    navLinks.classList.toggle('nav-open');
+window.addEventListener("scroll", function () {
+  scrollPosition = window.scrollY;
+
+  if (scrollPosition >= 30) {
+    logoContainer.classList.add("scrolled");
+  } else {
+    logoContainer.classList.remove("scrolled");
+  }
 });
+
+function loader() {
+  let body = document.querySelector("body");
+
+  body.classList.remove("loading");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
